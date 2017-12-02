66c208285e04bd039b8dfbc680a6490d3e4a4bd6
Rafael Timbó <timbo.rafa@gmail.com>
2017-12-01 19:17:39 -0500

    v2.0

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 function ajaxCall() {
-  $.getJSON("https://jsonplaceholder.typicode.com/posts", function fetchPosts(data) {
+  var data = $.getJSON("https://jsonplaceholder.typicode.com/posts", function fetchPosts(data) {
     $("#cardslist").append( data.map(function createCards(post) {
       //var card = document.createElement()
-      var card = $( "<hr><div class='card-body'</div>" )
+      var card = $( "<div class='card-body'</div>" )
       card.append(
-        $("<h4 class='card-title'>" + post.title + "</h4>"),
+        $("<h4 class='card-title'>" + post.title + " testtt</h4>"),
         $("<p class='card-text'>" + post.body + "</p>")
       )
       return card
-    }))
+    })[0])
+    console.log(data)
   })
 }
