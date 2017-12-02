4d08091fcb4c53a728880e77d480faaa7ca4ef8c
Rafael Timbó <timbo.rafa@gmail.com>
2017-12-01 19:31:20 -0500

    v3.0 done

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 function ajaxCall() {
-  var data = $.getJSON("https://jsonplaceholder.typicode.com/posts", function fetchPosts(data) {
+  // delete previous cards
+  $(".card-body, .card-rule, .card-text, .card-title").remove()
+  // fetch posts and include on html
+  $.getJSON("https://jsonplaceholder.typicode.com/posts", function fetchPosts(data) {
     $("#cardslist").append( data.map(function createCards(post) {
-      //var card = document.createElement()
-      var card = $( "<div class='card-body'</div>" )
+      var card = $( "<div class='card-body' </div>" )
+      card.prepend( "<hr class='card-rule'/>" )
       card.append(
-        $("<h4 class='card-title'>" + post.title + " testtt</h4>"),
+        $("<h4 class='card-title'>" + post.title + "</h4>"),
         $("<p class='card-text'>" + post.body + "</p>")
       )
       return card
-    })[0])
-    console.log(data)
+    }))
   })
 }
